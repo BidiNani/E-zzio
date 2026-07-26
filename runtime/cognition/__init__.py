fe7aceec6ade9b486c8f3861391126985acdfccd
BidiNani <enrik.pani@gmail.com>
2026-07-27 01:08:56 +0200

    feat(cognition): official release v2.5.0-cognitive-core with robust french apostrophe keyword extraction

---
+++ runtime/cognition/__init__.py
@@ -0,0 +1,5 @@
+"""E-ZZIO Cognition Layer"""
+from runtime.cognition.core import EzzioBrain
+from runtime.cognition.analyzer import TaskAnalyzer, TaskAnalysis
+
+__all__ = ["EzzioBrain", "TaskAnalyzer", "TaskAnalysis"]
---
+++ runtime/knowledge/__init__.py
@@ -0,0 +1,5 @@
+"""E-ZZIO Knowledge Layer"""
+from runtime.knowledge.models import KnowledgeItem, SourceType
+from runtime.knowledge.store import KnowledgeStore
+
+__all__ = ["KnowledgeItem", "SourceType", "KnowledgeStore"]
